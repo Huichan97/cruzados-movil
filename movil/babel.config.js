67f29cfe38ff5c1e381ea4374b29b8535d8cac42
Huichan97 <huichanfelipe@gmail.com>
2025-09-27 23:55:46 -0300

    configs

--- movil/babel.config.js
+++ movil/babel.config.js
@@ -4,12 +4,14 @@ module.exports = function (api) {
         presets: ['babel-preset-expo'],
         plugins: [
             [
-                "module:react-native-dotenv",
+                'module:react-native-dotenv',
                 {
-                    moduleName: "@env",
-                    path: ".env",
+                    moduleName: '@env',
+                    path: process.env.ENVFILE || '.env',
+                    safe: false,
+                    allowUndefined: true,
                 },
             ],
-        ],
+        ]
     };
 };
